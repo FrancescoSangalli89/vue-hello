ffa89c5224fe5e4b2210ec4c9556b1bd7c16d811
Francesco Sangalli <francesco.sangalli89@gmail.com>
2022-07-21 20:58:33 +0200

    finish: vue hello work

--- js/main.js
+++ js/main.js
@@ -4,7 +4,11 @@
 // Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 
 
-var app = new Vue({
+const app = new Vue({
     el: '#app',
-
+    data: {
+        title: 'Eccoci qua..',
+        myClass: 'title',
+        image: 'https://images.unsplash.com/photo-1542293787938-c9e299b880cc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8N3x8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60'
+    }
 })
